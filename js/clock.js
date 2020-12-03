cbf59c757ace4840a9e255746ee3e35bd2d5f75b
pinprojekt <pin.janos@gmail.com>
2020-12-03 15:09:06 +0100

    new clock

---
+++ js/clock.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+const getCurrentTime = () => {
+    const currentDate = new Date();
+    const hours = padNumbers(currentDate.getHours());
+    const minutes = padNumbers(currentDate.getMinutes());
+    const seconds = padNumbers(currentDate.getSeconds());
+
+    return `${[hours, minutes, seconds].join(':')}`;
+}
+
+const padNumbers = (num) => {
+    return num < 10 ? `0${num}` : `${num}`;
+}
+
+setInterval( () => {
+    const time = getCurrentTime();
+    
+    const clockFace = document.querySelector('.clock-face');
+    clockFace.textContent = time;
+
+}, 1000);
